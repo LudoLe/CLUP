3b1d64d92d610d0e894cf1f05b5e2e7c90822676
Federico Mainetti Gambera <federicomainettigambera@gmail.com>
2021-02-06 19:00:04 +0100

    react constants

---
+++ code/frontend/clup/src/constants/urlsAPI.js
@@ -0,0 +1,4 @@
+export const SSW_URL_API = "";
+export const QSW_URL_API = "";
+export const AMW_URL_API = "http://localhost:8080/AMW-1.0-SNAPSHOT/";
+export const ACCESS_TOKEN_NAME = 'login_access_token';
