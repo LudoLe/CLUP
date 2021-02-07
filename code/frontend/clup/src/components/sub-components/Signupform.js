3a71bb58c258aa14a489fde9885f9303d2a9d769
Federico Mainetti Gambera <federicomainettigambera@gmail.com>
2021-02-07 05:51:49 +0100

    refactor signupform react - frontend

--- code/frontend/clup/src/components/sub-components/Signupform.js
+++ code/frontend/clup/src/components/sub-components/Signupform.js
@@ -2,70 +2,117 @@ import React, { useState } from 'react';
 
 const Signupform = (props) => {
 
-    //TODO REFACTOR WITH STATE:
-    /* const [state, setState] = useState({
+    const [state, setState] = useState({
         nextBool: false,
         username: null,
         password: null,
+        repeatPassword: null,
         email: null,
         phonenumber: null,
         shopOwner: false
-    }); */
+    });
 
-    //TODO refactor all rendering with ternary operator
+    const handleChange = (e) => {
+        const {id , value} = e.target   
+        setState(prevState => ({
+            ...prevState,
+            [id] : value
+        }))
+    }
 
-    const [nextBool, setNextBool] = useState(false);
-    const [username, setUsername] = useState(null);
-    const [email, setEmail] = useState(null);
-    const [phonenumber, setPhonenumber] = useState(null);
-    const [password, setPassword] = useState(null);
-    const [shopOwner, setShopOwner] = useState(false);
-
-    const checkValidity = (event) => {
+    const checkValidity = (e) => {
         if (document.getElementById("signupForm").reportValidity()) {
-            setNextBool(true);
-            setUsername(document.getElementById("usernameInput").value);
-            setEmail(document.getElementById("emailInput").value);
-            setPhonenumber(document.getElementById("phonenumberInput").value);
-            setShopOwner(document.getElementById("shopOwner").checked);
+            setState(prevState => ({
+                ...prevState,
+                nextBool : true
+            }))
         }
     }
 
-    const submit = () => {
-        var pswd = document.getElementById("passwordInput");
-        var rptpswd = document.getElementById("repeatPasswordInput");
-
-        if (pswd.value !== rptpswd.value) {
-            rptpswd.setCustomValidity("Passwords Don't Match");
-        } else {
-            rptpswd.setCustomValidity('');
-            setPassword(pswd);
-            console.log("TODO, submit form..");
+    const handleSignupSubmit = (e) =>{
+        e.preventDefault();
+        if(e.target.reportValidity()) {
+            sendSignupToServer(); 
         }
+    }
+
+    const sendSignupToServer = () => {
+        if((state.username.length!=0) && (state.password.length!=0)) {
+            const payload={
+                "username":state.username,
+                "password":state.password
+            }
+            axios.post(AMW_URL_API+'/login', payload)
+                .then(function (response) {
+                    if(response.status === 200){
+                        setState(prevState => ({
+                            ...prevState,
+                            'successMessage' : 'Registration successful. Redirecting to home page..'
+                        }))
+                        console.log(response); //JUSTO TO VISUALIZE IT
+                        localStorage.setItem(ACCESS_TOKEN_NAME, response.data.token); //SAVE LOGIN TOKEN
+                        redirectToHome();
+                    } else{
+                        console.log("HERE");
+                        setState(prevState => ({
+                            ...prevState,
+                            'error' : {
+                                status: response.status,
+                                message: response.message
+                            }
+                        }))
+                    }
+                })
+                .catch(function (error) {
+                    setState(prevState => ({
+                        ...prevState,
+                        'error' : error
+                    }))
+                    console.log(error);
+                });    
+        } else {
+            props.showError('Please enter valid username and password')    
+        }
+    }
+
+    const redirectToHome = () => {
+        window.location.href = "/Home";
     }
 
     const renderCorrectInputContainer = () => {
-        if (!nextBool) {
+        if (!state.nextBool) {
             return (
                 <div className="inputContainer">
                     <h3>Create a new account</h3>
                     <div className="flexColumnCenter">
                         <label className="labelSignup">Username</label>
-                        <input id="usernameInput" type="text" placeholder="Username" name="username" required />
+                        <input onChange={handleChange}
+                            value={state.username}
+                            id="username" 
+                            type="text" placeholder="Username" name="username" required />
                     </div>
                     <div className="flexColumnCenter">
                         <label className="labelSignup">Email</label>
-                        <input id="emailInput" type="email" placeholder="email@example.com" name="email" required />
+                        <input onChange={handleChange}
+                            value={state.email}
+                            id="email" 
+                            type="email" placeholder="email@example.com" name="email" required />
                     </div>
                     <div className="flexColumnCenter">
                         <label className="labelSignup">Phone number</label>
-                        <input id="phonenumberInput" type='number' placeholder="+39 0123456789" name="phoneNumber" required />
+                        <input onChange={handleChange}
+                            value={state.phonenumber}
+                            id="phonenumber" 
+                            type='number' placeholder="+39 0123456789" name="phoneNumber" required />
                     </div>
                     <div className="flexColumnCenter">
                         <label className="labelSignup">Are you a shop owner?</label>
-                        <input id="shopOwner" type="checkbox" name="shopOwner" />
+                        <input onChange={handleChange}
+                            value={state.shopOwner}
+                            id="shopOwner"
+                            type="checkbox" name="shopOwner" />
                     </div>
-                    <button className="activeButton" type="submit" onClick={checkValidity}>Next</button>
+                    <button className="activeButton" onClick={checkValidity}>Next</button>
                 </div>
             );
         }
@@ -75,15 +122,20 @@ const Signupform = (props) => {
                     <h3>Create a password</h3>
                     <div className="flexColumnCenter">
                         <label className="labelSignup">Password</label>
-                        <input id="passwordInput" type="password" placeholder="Password" name="password" required />
+                        <input onChange={handleChange}
+                            value={state.password}
+                            id="password"
+                            type="password" placeholder="Password" name="password" required />
                     </div>
                     <div className="flexColumnCenter">
                         <label className="labelSignup">Repeat password</label>
-                        <input id="repeatPasswordInput" type="password" placeholder="Repeat password" name="repeatPassword" required />
+                        <input onChange={handleChange}
+                            value={state.repeatPassword}
+                            id="repeatPassword" type="password" placeholder="Repeat password" name="repeatPassword" required />
                     </div>
                     <div className="flexRowCenter">
-                        <button className="activeButton" type="submit" onClick={() => { setNextBool(false) }}> Back </button>
-                        <button className="activeButton" onClick={submit}>Confirm</button>
+                        <button className="activeButton" type="submit" onClick={() => { setState({ nextBool: false }}> Back </button>
+                        <button className="activeButton">Confirm</button>
                     </div>
                 </div>
             );
@@ -93,7 +145,7 @@ const Signupform = (props) => {
     if (props.open) {
         return (
             <div className="loginFormContainer">
-                <form id="signupForm" autoComplete="off">
+                <form onSubmit={(state.nextBool === true) ? {handleSignupSubmit} : ""} id="signupForm" autoComplete="off">
                     {renderCorrectInputContainer()}
                 </form>
             </div>
