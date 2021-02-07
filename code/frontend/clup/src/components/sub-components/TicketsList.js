d7327425e4b0e9b647ebbb0b834db6d3d91e3eed
Federico Mainetti Gambera <federicomainettigambera@gmail.com>
2021-02-07 07:42:02 +0100

    Home Page tickets list component structure - frontend

--- code/frontend/clup/src/components/sub-components/TicketsList.js
+++ code/frontend/clup/src/components/sub-components/TicketsList.js
@@ -1,13 +1,7 @@
 import React from 'react';
-import TicketElement from './TicketElement'
 
 const TicketsList = () => {
-    return (
-        <div>
-            <h3> TODO: list of tickets. </h3>
-            <TicketElement />
-        </div>
-    );
+    return <div> lista di biglietti qua </div>
 }
 
 export default TicketsList;
