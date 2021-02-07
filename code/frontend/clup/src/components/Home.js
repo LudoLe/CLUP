b908a6adc1413a13280fe17899e079ab15967b29
Federico Mainetti Gambera <federicomainettigambera@gmail.com>
2021-02-07 07:41:36 +0100

    Home page user structure - frontend

--- code/frontend/clup/src/components/Home.js
+++ code/frontend/clup/src/components/Home.js
@@ -1,15 +1,68 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Searchsection from './sub-components/Searchsection';
 import TicketsList from './sub-components/TicketsList';
 import Account from './sub-components/Account';
+import { AMW_URL_API, SSW_URL_API, ACCESS_TOKEN_NAME } from '../constants/urlsAPI';
 
 const Home = () => {
 
+    const [state, setState] = useState({
+        isLoadedTickets: false,
+        isLoadedAccount: false,
+        tickets: [],
+        error: [],
+        account: []
+    });
+
+    //fetch ticket info
+    useEffect(()=>{
+        fetch(SSW_URL_API + "/boh")
+        .then(res => res.json())
+        .then(
+            (result) => {
+                setState(prevState => ({
+                    ...prevState,
+                    isLoadedTickets: true,
+                    tickets: result
+                }));
+            },
+            (error) => {
+                setState(prevState => ({
+                    ...prevState,
+                    isLoadedTickets: true,
+                    error: error
+                }));
+            }
+        )
+    }, []); 
+
+    //fetch account info
+    useEffect(()=>{
+        fetch(AMW_URL_API + "/userinfo") //TODO
+        .then(res => res.json())
+        .then(
+            (result) => {
+                setState(prevState => ({
+                    ...prevState,
+                    isLoadedAccount: true,
+                    account: result
+                }));
+            },
+            (error) => {
+                setState(prevState => ({
+                    ...prevState,
+                    isLoadedAccount: true,
+                    error: error
+                }));
+            }
+        )
+    }, []); 
+
     return (
         <div>
-            <Account />
+            <Account isLoaded={state.isLoadedAccount} account={state.account} />
             <Searchsection />
-            <TicketsList />
+            <TicketsList isLoaded={state.isLoadedTickets} tickets={state.tickets} />
         </div>
     );
 }
