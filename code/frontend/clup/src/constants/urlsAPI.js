c7c5e6499917ad0ca13d3c1524f40e9d7f4fff12
Federico Mainetti Gambera <federicomainettigambera@gmail.com>
2021-02-07 07:40:58 +0100

    update api url  - frontend

--- code/frontend/clup/src/constants/urlsAPI.js
+++ code/frontend/clup/src/constants/urlsAPI.js
@@ -1,4 +1,4 @@
-export const SSW_URL_API = "";
-export const QSW_URL_API = "";
+export const SSW_URL_API = "http://localhost:8081/SSW-1.0-SNAPSHOT/api/SSW";
+export const QSW_URL_API = "http://localhost:8083/QSW-1.0-SNAPSHOT/api/QSW";
 export const AMW_URL_API = "http://localhost:8082/AMW-1.0-SNAPSHOT/api/AMW";
 export const ACCESS_TOKEN_NAME = 'login_access_token';
