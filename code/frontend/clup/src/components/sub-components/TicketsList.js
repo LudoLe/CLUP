24f00db0be9d334d2beea5c795597f2848013115
Federico Mainetti Gambera <federicomainettigambera@gmail.com>
2021-02-07 16:11:46 +0100

    Home page - frontend

--- code/frontend/clup/src/components/sub-components/TicketsList.js
+++ code/frontend/clup/src/components/sub-components/TicketsList.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import TicketElement from './TicketElement';
+import Loading from '@bit/webnovel.read-ui.loading';
 
-const TicketsList = () => {
-    return <div> lista di biglietti qua </div>
+const TicketsList = (props) => {
+    if(props.isLoadedTickets){
+        return <div className="flexColumnCenter"> {props.tickets.map(ticket => <TicketElement ticket={ticket} />)} </div>
+    }
+    else{
+        return <div className="flexColumnCenter"> <Loading/> </div>
+    }
 }
 
 export default TicketsList;
