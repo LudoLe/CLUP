cb4cf3901c91e3497ee9310c686e7456ab1020cc
Federico Mainetti Gambera <federicomainettigambera@gmail.com>
2021-02-07 06:10:10 +0100

    change port and url for QSW

--- code/backend/QSW/src/main/java/polimi/it/QSW/Root.java
+++ code/backend/QSW/src/main/java/polimi/it/QSW/Root.java
@@ -13,8 +13,8 @@ public class Root extends Application {
         BeanConfig beanConfig = new BeanConfig();
         beanConfig.setVersion("1.0.0");
         beanConfig.setSchemes(new String[]{"http"});
-        beanConfig.setHost("localhost:8080");
-        beanConfig.setBasePath("/QSW_war_exploded/api");
+        beanConfig.setHost("localhost:8083");
+        beanConfig.setBasePath("/QSW-1.0-SNAPSHOT/api");
         beanConfig.setResourcePackage(Gateway.class.getPackage().getName());
         beanConfig.setTitle("Information Service");
         beanConfig.setDescription("A simple information service");
