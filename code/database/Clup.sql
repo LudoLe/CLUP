87af1c8d0510e7863795bf84668c00b9169079db
Federico Mainetti Gambera <federicomainettigambera@gmail.com>
2021-02-08 19:30:32 +0100

    Update Clup.sql

--- code/database/Clup.sql
+++ code/database/Clup.sql
@@ -35,7 +35,7 @@ CREATE TABLE `shop` (
   PRIMARY KEY (`id`),
   KEY `manager_id_idx` (`manager_id`),
   CONSTRAINT `manager_id` FOREIGN KEY (`manager_id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -44,6 +44,7 @@ CREATE TABLE `shop` (
 
 LOCK TABLES `shop` WRITE;
 /*!40000 ALTER TABLE `shop` DISABLE KEYS */;
+INSERT INTO `shop` VALUES (1,'string','string','string','string',3,0,0,0),(2,'new shop','string','string','string',7,0,0,0),(3,'new shop','string','string','string',7,0,0,0),(4,'str0ing','string','string','string',4,0,-5,0),(5,'string','string','string','string',4,3,3,3),(6,'string','string','string','string',4,3,3,34),(7,'string','string','string','string',4,10,10,10),(8,'shoppi','shoppi ','ssss','string',4,5,5,5);
 /*!40000 ALTER TABLE `shop` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -59,11 +60,11 @@ CREATE TABLE `shop_shift` (
   `shop_id` int NOT NULL,
   `opening_time` time NOT NULL,
   `closing_time` time NOT NULL,
-  `day` varchar(45) NOT NULL,
+  `day` int NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   KEY `related_shop_idx` (`shop_id`),
   CONSTRAINT `shop_schedule` FOREIGN KEY (`shop_id`) REFERENCES `shop` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -72,6 +73,7 @@ CREATE TABLE `shop_shift` (
 
 LOCK TABLES `shop_shift` WRITE;
 /*!40000 ALTER TABLE `shop_shift` DISABLE KEYS */;
+INSERT INTO `shop_shift` VALUES (1,5,'13:44:50','13:44:50',0),(2,5,'13:50:43','13:50:43',0),(3,5,'13:50:43','13:50:43',0),(4,5,'13:50:43','13:50:43',0),(5,4,'16:17:16','16:17:16',0);
 /*!40000 ALTER TABLE `shop_shift` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -92,12 +94,13 @@ CREATE TABLE `ticket` (
   `expected_duration` time NOT NULL,
   `scheduled_entering_time` time DEFAULT NULL,
   `scheduled_exiting_time` time DEFAULT NULL,
+  `time_to_reach_the_shop` time DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `relate_shop_idx` (`shop_id`),
   KEY `user_ticket_idx` (`user_id`),
   CONSTRAINT `related_shop_ticket` FOREIGN KEY (`shop_id`) REFERENCES `shop` (`id`),
   CONSTRAINT `user_ticket` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -106,6 +109,7 @@ CREATE TABLE `ticket` (
 
 LOCK TABLES `ticket` WRITE;
 /*!40000 ALTER TABLE `ticket` DISABLE KEYS */;
+INSERT INTO `ticket` VALUES (21,5,4,'VALID','13:50:43','13:50:43','00:00:45','13:50:43','13:50:43','00:00:12'),(22,5,4,'INVALID','13:50:43','13:50:43','00:00:56','13:50:43','13:50:43','00:00:54'),(23,4,5,'invalid',NULL,NULL,'00:00:55',NULL,NULL,'19:00:01'),(24,5,5,'invalid',NULL,NULL,'00:14:01',NULL,NULL,'00:14:01'),(25,5,5,'invalid',NULL,NULL,'20:00:06',NULL,NULL,'20:00:06'),(26,5,5,'invalid',NULL,NULL,'20:00:14',NULL,NULL,'20:00:14'),(27,5,5,'invalid',NULL,NULL,'20:00:34',NULL,NULL,'20:00:34'),(28,5,4,'invalid',NULL,NULL,'00:08:10',NULL,NULL,'15:00:10'),(29,5,4,'invalid',NULL,NULL,'00:11:49',NULL,NULL,'00:11:49'),(30,5,4,'invalid',NULL,NULL,'00:16:52',NULL,NULL,'00:16:52'),(31,5,4,'invalid',NULL,NULL,'00:24:28',NULL,NULL,'00:24:28');
 /*!40000 ALTER TABLE `ticket` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -123,9 +127,9 @@ CREATE TABLE `user` (
   `email` varchar(45) NOT NULL,
   `phone_number` varchar(15) NOT NULL,
   `is_manager` tinyint(1) NOT NULL DEFAULT '0',
-  `session_token` varchar(45) NOT NULL,
+  `session_token` varchar(32) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -134,7 +138,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'ludole','$argon2id$v=19$m=16384,t=2,p=1$FmHxp1kiTp0yjjGVkwzSlD8CmOarq93Ko+z95LHbqhhHsfL4C3gYxTSekDK7rpiyIF5ivnRfJqLvXyKBhPNvkA$ea2rw44ymoe8wZMARRsn7kRMI19j2ccl/SNZAxs2J+Xn6gpKBYurKXJpqGA2yxggeVPBKbsAyTRHWMH0thYmGDHlU0vQ7cZYOyi/3dHt8iK+a1mh4MMFLYeZlofBJNM/P9+7wXJT3EFN+iOM33vtVBwaAwSG7bc5T3rrmkQ9zQY','ludole@ludolu.com','333',1,''),(2,'ludol3e3','$argon2id$v=19$m=16384,t=2,p=1$Hq0f2Uta+SSOVznnQHCq7E8Kkfi6Cbxryu4x60kGTJthB+egTbt6lUztq5oqgxcSJ1+gfpMvCSobaqfYNLFQMQ$+9nrz4ldsYjzTW07jCSYSd84ZfHrVmc9WiVQIQNBFLUe3llU+vDh0jy0nYvRHuSAbaEBnRlWk5oGDBrwRdxaJPpJ89BaXdMSjhqFyAzkkgy48QTu1EymVx4yDu/zmxcSunlG5izvX2mz5w1N6r401u8EHl6SagaNO26soLdl7dw','ludole@ludolu3.com','3333',1,'');
+INSERT INTO `user` VALUES (1,'ludole','$argon2id$v=19$m=16384,t=2,p=1$FmHxp1kiTp0yjjGVkwzSlD8CmOarq93Ko+z95LHbqhhHsfL4C3gYxTSekDK7rpiyIF5ivnRfJqLvXyKBhPNvkA$ea2rw44ymoe8wZMARRsn7kRMI19j2ccl/SNZAxs2J+Xn6gpKBYurKXJpqGA2yxggeVPBKbsAyTRHWMH0thYmGDHlU0vQ7cZYOyi/3dHt8iK+a1mh4MMFLYeZlofBJNM/P9+7wXJT3EFN+iOM33vtVBwaAwSG7bc5T3rrmkQ9zQY','ludole@ludolu.com','333',1,''),(2,'ludol3e3','$argon2id$v=19$m=16384,t=2,p=1$Hq0f2Uta+SSOVznnQHCq7E8Kkfi6Cbxryu4x60kGTJthB+egTbt6lUztq5oqgxcSJ1+gfpMvCSobaqfYNLFQMQ$+9nrz4ldsYjzTW07jCSYSd84ZfHrVmc9WiVQIQNBFLUe3llU+vDh0jy0nYvRHuSAbaEBnRlWk5oGDBrwRdxaJPpJ89BaXdMSjhqFyAzkkgy48QTu1EymVx4yDu/zmxcSunlG5izvX2mz5w1N6r401u8EHl6SagaNO26soLdl7dw','ludole@ludolu3.com','3333',1,''),(3,'string','$argon2id$v=19$m=16384,t=2,p=1$+iiZUHDZoZMt0jWTo3hoLSIbZlJv9kyjv/WRDysPBjVv75STfwZKCbon4m2l5gMTRDb5LCi5jtah/J9UJwUlzg$F7nHu6DNE56MtbK2vOpdjaEFEjUn1Sw8IxEnZ5Cihaj4X6WRSwvr1jITRIE23FEPHT0SZqPEwlup4CjhEiKxvOldrfzFy2YUTkh+D+EBdhM8wI3vF9tXx9XweiSSiV0748T1irA6EuEQHZQv5eXyaCXpTpCQkMsB9Sqeh7eQW04','string','string',0,'R3JoHSJP0833x-6nn33WsgZCoU4myxI5'),(4,'string3','$argon2id$v=19$m=16384,t=2,p=1$dinDHhsDX+VkJyZ4Fv5B1qynHcglapyflKwk/KmoQj5cC3d7KAWPhkKVAphXOK4UfvhU+MLQAeEsKX9Z7/gZ/Q$vIV/LAlIIcQh8lrlVwR23clLAUTx7gA1lu77sX5wShI1kW41JVTqVD2gs0tPt42VOGD1sNcWk5OUjkNxtVgrSpfq2wB6nG31eDzkwf2/xOvlDcBu5W3Gy7rqiNQ3/xaRjI6viAWl5JSRB/yM0e/kElEguhTDfmEKyNMSRQm1UIw','string2','string',1,'gIz5BnTMRpmPalmgzRmznI4CrdtITlSc'),(5,'string4','$argon2id$v=19$m=16384,t=2,p=1$9cKqu6woTXkmFuHcVZnkfkJKmEbEvs9mtyQGXJ8c5STW9anSBI35Q+jYt1WZY/WZ2gEN7zGFChM7OtKkWV+2AA$KZ4GHdFVFunADm4xT9jFGJzl1CSpvLXUNJdJGDFpQIzbDHdoRGJmHSaKVAMA/fmpJeFVlAYv3jQfuoyqe9u6ole2ZhtapwSkQSEYhPTNjr7XGby0tDNiHm1Q777gtsFPKDdABrdGHjawD8vJxGIceFHJDZEibZcIfYElYquwgRo','string5','string',1,'YXixs50V7AISz_D4ToEGITqcbgSysURo'),(6,'string5','$argon2id$v=19$m=16384,t=2,p=1$ho9Co61t1yst+S7j3Y4g4yOEgDefGzV+xztYtccs9XkSqfHlsy+SUWxWLtkSwwklYodS7lrBI9ER8z1O+HEnMA$37BwgfU9OFzbnSDQdhKqyMIpEQ96JtL+TDS9fg5qcga8zBchlDgbiQ2kTVToFIj+WG7AIFQ3UApYg2qTKtLnpC5QpH0KRA+77RR/T+XcOealwj29U2OSAPC0bjc5c+4/BPZQTk1tuSnNJTDUml5kkoOnY8GJ3Qm3gEGa1h0DL1s','string55','string',1,''),(7,'string0','$argon2id$v=19$m=16384,t=2,p=1$4/1Yrs+o/SYx57Xl0yPloTAadSrLI4OcUf5P5hVxFxM7fKj8SPfFjhvMLyjAaiaEFZ7fR6rgSm9fs5TnsX7ncw$gEMPZchukR3GoKgH2Kh3OSV8Ubg1FUja/asj6uSyUOFDNk0LTSviah91mj9I+zJDhcdx5iSlsQRTpskE8C0eaVpxwfXsqqV7IkJ0kOQTzvSqJGT709xg3Crul1WzRFgn9eha/cS27Pb955rJGsQXNhs5oH61n9VSoZmKA2pPE/M','string12','string',1,NULL),(8,'lotto','$argon2id$v=19$m=16384,t=2,p=1$klKu6Bkikqs1Icp0ElBGI20HUEqccMeX3tI2M0bwkXUoMRTia2WkC3s9/mKRljQUYWQ3s6C008P02OwXvi28Tg$OsoxpIJmFDoIUi8m/CyuVokRRlJGqTuH8Fe2De8O7g6A1LaMCJNJwHS7kWPsJjY1XamOKA06zjzz8MJXiG+RQsqd/dGbSoIqPhrbZ7ILIbFd37UNJj5mDGGtI0Vhj9Smd51H9SRqdfHkTyx5IeVWbbyG3/g+qCoNS/UChdva928','slottog','string',0,'4iLjLW9oDV3EZSv3yyv3IrQbt_kxENZJ');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -147,4 +151,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-02-06  2:07:48
+-- Dump completed on 2021-02-08 19:26:57
