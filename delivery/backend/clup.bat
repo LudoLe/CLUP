2d7f19c1e4f8a36b61f847ab8c6c50aa22aaddd8
Federico Mainetti Gambera <federicomainettigambera@gmail.com>
2021-02-11 19:42:38 +0100

    backend build delivery

---
+++ delivery/backend/clup.bat
@@ -0,0 +1,18 @@
+cd AMW
+cd bin
+start call startup.bat
+cd ..
+cd ..
+cd SSW
+cd bin
+start call startup.bat
+cd ..
+cd ..
+cd QSW
+cd bin
+start call startup.bat
+cd ..
+cd ..
+cd Data_server
+cd bin
+start call mysqld.exe
